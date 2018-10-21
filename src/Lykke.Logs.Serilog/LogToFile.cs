1175933440f09700d3952e99eb0554134f7d93a1
Alexey Andreev <alexey.andreev@lykke.com>
2018-10-21 13:30:24 +0300

    MTC-391: use separate default serilog config

--- src/Lykke.Logs.Serilog/LogToFile.cs
+++ src/Lykke.Logs.Serilog/LogToFile.cs
@@ -5,69 +5,64 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Common.Log;
 using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.PlatformAbstractions;
 using Serilog;
 using Serilog.Core;
-using LogLevel = Lykke.Logs.Serilog.Domain.LogLevel;
+using Serilog.Events;
 
 namespace Lykke.Logs.Serilog
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [UsedImplicitly]
     public class LogToFile : ILog
     {
         private readonly Logger _logger;
+        private readonly string _logName;
 
-        public LogToFile(Assembly assembly, SerilogSettings serilogSettings, bool logToConsole)
+        public LogToFile(IConfiguration configuration, string logName = null)
         {
+            var assembly = configuration.GetType().Assembly;
             var title = assembly.Attribute<AssemblyTitleAttribute>(attribute => attribute.Title);
             var version = assembly.Attribute<AssemblyInformationalVersionAttribute>(attribute => attribute.InformationalVersion);
             var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
             
-            var configKeyValuePairs = serilogSettings.ToKeyValuePairs()
-                .Append(new KeyValuePair<string, string>("Application", title))
-                .Append(new KeyValuePair<string, string>("Version", version))
-                .Append(new KeyValuePair<string, string>("Environment", environmentName));
-            
-            var loggerConfig = new LoggerConfiguration()
-                .ReadFrom.KeyValuePairs(configKeyValuePairs);
-            if (logToConsole)
-            {
-                loggerConfig.WriteTo.Console();
-            }
-            _logger = loggerConfig
+//            var configKeyValuePairs = serilogSettings.ToKeyValuePairs()
+//                .Append(new KeyValuePair<string, string>("Application", title))
+//                .Append(new KeyValuePair<string, string>("Version", version))
+//                .Append(new KeyValuePair<string, string>("Environment", environmentName));
+//            
+//            var loggerConfig = new LoggerConfiguration()
+//                .ReadFrom.KeyValuePairs(configKeyValuePairs);
+//            if (logToConsole)
+//            {
+//                loggerConfig.WriteTo.Console();
+//            }
+//            _logger = loggerConfig
+//                .CreateLogger();
+            _logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.WithProperty("Application", title)
+                .Enrich.WithProperty("Version", version)
+                .Enrich.WithProperty("Environment", environmentName)
                 .CreateLogger();
+
+            _logName = logName ?? $"{title}Log";
         }
 
-        private Task WriteLog(LogLevel level, string component, string process, string context, string info, 
+        private Task WriteLog(LogEventLevel level, string component, string process, string context, string info, 
             Exception ex = null, DateTime? dateTime = null)
         {
 #pragma warning disable 1998
             Task.Run(async () =>
 #pragma warning restore 1998
             {
-                var time = dateTime ?? DateTime.UtcNow;
-                var message = $"{time:yyyy-MM-dd HH:mm:ss:fff} [{level}] {component}:{process}:{context} - {info}";
-
-                switch (level)
-                {
-                    case LogLevel.Info:
-                        _logger.Information(message);
-                        break;
-                    case LogLevel.Warning:
-                        _logger.Warning(ex, message);
-                        break;
-                    case LogLevel.Error:
-                        _logger.Error(ex, message);
-                        break;
-                    case LogLevel.FatalError:
-                        _logger.Fatal(ex, message);
-                        break;
-                    case LogLevel.Monitoring:
-                        _logger.Verbose(message);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(level), level, null);
-                }
+                //var time = dateTime ?? DateTime.UtcNow;
+                //var message = $"{time:yyyy-MM-dd HH:mm:ss:fff} [{level}] {component}:{process}:{context} - {info}";
+                
+                _logger.Write(level, ex, message);
             });
 
             return Task.CompletedTask;
@@ -75,64 +70,64 @@ namespace Lykke.Logs.Serilog
         
         public async Task WriteInfoAsync(string component, string process, string context, string info, DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.Info, component, process, context, info, null, dateTime);
+            await WriteLog(LogEventLevel.Information, component, process, context, info, null, dateTime);
         }
 
         public async Task WriteMonitorAsync(string component, string process, string context, string info, DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.Monitoring, component, process, context, info, null, dateTime);
+            await WriteLog(LogEventLevel.Verbose, component, process, context, info, null, dateTime);
         }
 
         public async Task WriteWarningAsync(string component, string process, string context, string info, DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.Warning, component, process, context, info, null, dateTime);
+            await WriteLog(LogEventLevel.Warning, component, process, context, info, null, dateTime);
         }
 
         public async Task WriteWarningAsync(string component, string process, string context, string info, Exception ex,
             DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.Warning, component, process, context, info, ex, dateTime);
+            await WriteLog(LogEventLevel.Warning, component, process, context, info, ex, dateTime);
         }
 
         public async Task WriteErrorAsync(string component, string process, string context, Exception exception, DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.Error, component, process, context, null, exception, dateTime);
+            await WriteLog(LogEventLevel.Error, component, process, context, null, exception, dateTime);
         }
 
         public async Task WriteFatalErrorAsync(string component, string process, string context, Exception exception,
             DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.FatalError, component, process, context, null, exception, dateTime);
+            await WriteLog(LogEventLevel.Fatal, component, process, context, null, exception, dateTime);
         }
 
         public async Task WriteInfoAsync(string process, string context, string info, DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.Info, null, process, context, info, null, dateTime);
+            await WriteLog(LogEventLevel.Information, null, process, context, info, null, dateTime);
         }
 
         public async Task WriteMonitorAsync(string process, string context, string info, DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.Monitoring, null, process, context, info, null, dateTime);
+            await WriteLog(LogEventLevel.Verbose, null, process, context, info, null, dateTime);
         }
 
         public async Task WriteWarningAsync(string process, string context, string info, DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.Warning, null, process, context, info, null, dateTime);
+            await WriteLog(LogEventLevel.Warning, null, process, context, info, null, dateTime);
         }
 
         public async Task WriteWarningAsync(string process, string context, string info, Exception ex, DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.Warning, null, process, context, info, ex, dateTime);
+            await WriteLog(LogEventLevel.Warning, null, process, context, info, ex, dateTime);
         }
 
         public async Task WriteErrorAsync(string process, string context, Exception exception, DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.Error, null, process, context, null, exception, dateTime);
+            await WriteLog(LogEventLevel.Error, null, process, context, null, exception, dateTime);
         }
 
         public async Task WriteFatalErrorAsync(string process, string context, Exception exception, DateTime? dateTime = null)
         {
-            await WriteLog(LogLevel.FatalError, null, process, context, null, exception, dateTime);
+            await WriteLog(LogEventLevel.Fatal, null, process, context, null, exception, dateTime);
         }
     }
 }
