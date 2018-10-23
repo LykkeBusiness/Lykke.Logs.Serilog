3a50f65f17ecf1383935e1e1d46d417401fb119e
Alexey Andreev <alexey.andreev@lykke.com>
2018-10-23 12:07:41 +0300

    MTC-391: parameters passed via Enrichers, config extension to change log name

---
+++ src/Lykke.Logs.Serilog.TestClient/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Lykke.Logs.Serilog;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace Lykke.Logs.Serilog.TestClient
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+        
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddSerilogJson(env)
+                .AddEnvironmentVariables()
+                .Build();
+            
+            Configuration = Configuration.WithSubstitutions(new Dictionary<string, string> {{"{LogName}", "testLogReplaced"}});
+
+            Environment = env;
+        }
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var logger = new LogToFile(GetType().Assembly, Configuration, "testLog");
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+                await logger.WriteInfoAsync(nameof(TestClient), nameof(Startup), "Test logging 1111");
+            });
+        }
+    }
+}
--- src/Lykke.Logs.Serilog/LogToFile.cs
+++ src/Lykke.Logs.Serilog/LogToFile.cs
@@ -9,22 +9,24 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.PlatformAbstractions;
 using Serilog;
+using Serilog.Context;
 using Serilog.Core;
+using Serilog.Core.Enrichers;
 using Serilog.Events;
 
 namespace Lykke.Logs.Serilog
 {
     /// <summary>
-    /// 
+    /// ILog wrapper to write to files. Hides Serilog implementation behind.
+    /// Serilog must be configured with appsettings.Serilog.json.
     /// </summary>
     [UsedImplicitly]
     public class LogToFile : ILog
     {
         private readonly Logger _logger;
 
-        public LogToFile(IConfiguration configuration, string logName = null)
+        public LogToFile(Assembly assembly, IConfiguration configuration)
         {
-            var assembly = configuration.GetType().Assembly;
             var title = assembly.Attribute<AssemblyTitleAttribute>(attribute => attribute.Title);
             var version = assembly.Attribute<AssemblyInformationalVersionAttribute>(attribute => attribute.InformationalVersion);
             var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
@@ -34,8 +36,10 @@ namespace Lykke.Logs.Serilog
                 .Enrich.WithProperty("Application", title)
                 .Enrich.WithProperty("Version", version)
                 .Enrich.WithProperty("Environment", environmentName)
-                .Enrich.WithProperty("LogName", logName ?? $"{title}Log")
+                .Enrich.FromLogContext()
                 .CreateLogger();
+
+            WriteLog(LogEventLevel.Information, title, version, environmentName, "Started logging.");
         }
 
         private Task WriteLog(LogEventLevel level, string component, string process, string context, string info, 
@@ -45,10 +49,14 @@ namespace Lykke.Logs.Serilog
             Task.Run(async () =>
 #pragma warning restore 1998
             {
-                var time = dateTime ?? DateTime.UtcNow;
-                var message = $"{time:yyyy-MM-dd HH:mm:ss:fff} [{level}] {component}:{process}:{context} - {info}";
-                
-                _logger.Write(level, ex, message);
+                using (LogContext.Push(
+                    new PropertyEnricher("Component", component), 
+                    new PropertyEnricher("Process", process),
+                    new PropertyEnricher("Context", context)
+                    ))
+                {
+                    _logger.Write(level, ex, info);
+                }
             });
 
             return Task.CompletedTask;
@@ -88,32 +96,32 @@ namespace Lykke.Logs.Serilog
 
         public async Task WriteInfoAsync(string process, string context, string info, DateTime? dateTime = null)
         {
-            await WriteLog(LogEventLevel.Information, null, process, context, info, null, dateTime);
+            await WriteLog(LogEventLevel.Information, string.Empty, process, context, info, null, dateTime);
         }
 
         public async Task WriteMonitorAsync(string process, string context, string info, DateTime? dateTime = null)
         {
-            await WriteLog(LogEventLevel.Verbose, null, process, context, info, null, dateTime);
+            await WriteLog(LogEventLevel.Verbose, string.Empty, process, context, info, null, dateTime);
         }
 
         public async Task WriteWarningAsync(string process, string context, string info, DateTime? dateTime = null)
         {
-            await WriteLog(LogEventLevel.Warning, null, process, context, info, null, dateTime);
+            await WriteLog(LogEventLevel.Warning, string.Empty, process, context, info, null, dateTime);
         }
 
         public async Task WriteWarningAsync(string process, string context, string info, Exception ex, DateTime? dateTime = null)
         {
-            await WriteLog(LogEventLevel.Warning, null, process, context, info, ex, dateTime);
+            await WriteLog(LogEventLevel.Warning, string.Empty, process, context, info, ex, dateTime);
         }
 
         public async Task WriteErrorAsync(string process, string context, Exception exception, DateTime? dateTime = null)
         {
-            await WriteLog(LogEventLevel.Error, null, process, context, null, exception, dateTime);
+            await WriteLog(LogEventLevel.Error, string.Empty, process, context, null, exception, dateTime);
         }
 
         public async Task WriteFatalErrorAsync(string process, string context, Exception exception, DateTime? dateTime = null)
         {
-            await WriteLog(LogEventLevel.Fatal, null, process, context, null, exception, dateTime);
+            await WriteLog(LogEventLevel.Fatal, string.Empty, process, context, null, exception, dateTime);
         }
     }
 }
