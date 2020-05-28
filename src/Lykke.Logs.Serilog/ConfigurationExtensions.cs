14d8aff0225a48920b31e2eeffeacccb5cdd9439
Andrei Tarutin <tarutin_andrew@mail.ru>
2020-05-28 13:11:44 +0300

    updated packages

--- src/Lykke.Logs.Serilog/ConfigurationExtensions.cs
+++ src/Lykke.Logs.Serilog/ConfigurationExtensions.cs
@@ -1,10 +1,15 @@
+using System;
 using System.IO;
 using JetBrains.Annotations;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace Lykke.Logs.Serilog
 {
+    /// <summary>
+    /// Serilog configuration extensions
+    /// </summary>
     public static class ConfigurationExtensions
     {
         /// <summary>
@@ -14,7 +19,20 @@ namespace Lykke.Logs.Serilog
         /// <param name="env"></param>
         /// <returns></returns>
         [UsedImplicitly]
+        [Obsolete("Use AddSerilogJson with IHostEnvironment parameter instead")]
         public static IConfigurationBuilder AddSerilogJson(this IConfigurationBuilder builder, IHostingEnvironment env)
+        {
+            return builder.AddJsonFile(Path.Combine(env.ContentRootPath, "appsettings.Serilog.json"));
+        }
+        
+        /// <summary>
+        /// Add serilog config to configuration from appsettings.Serilog.json file.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="env"></param>
+        /// <returns></returns>
+        [UsedImplicitly]
+        public static IConfigurationBuilder AddSerilogJson(this IConfigurationBuilder builder, IHostEnvironment env)
         {
             return builder.AddJsonFile(Path.Combine(env.ContentRootPath, "appsettings.Serilog.json"));
         }
