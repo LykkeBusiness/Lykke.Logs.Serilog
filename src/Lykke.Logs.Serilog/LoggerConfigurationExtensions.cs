ea3281674c45acebbf9bc0e1d0681d9495f9c89f
atarutin <andrey.tarutin@lykke-business.ch>
2022-09-23 14:08:49 +0300

    fix(LT-4158): Initializing Serilog logger depending on environment

---
+++ src/Lykke.Logs.Serilog/LoggerConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using Serilog;
+
+namespace Lykke.Logs.Serilog;
+
+public static class LoggerConfigurationExtensions
+{
+    /// <summary>
+    /// Creates serilog logger depending on the environment
+    /// There is a known issue <see href="// https://github.com/serilog/serilog-aspnetcore/issues/289">here</see>
+    /// so that we can not use CreateBootstrapLogger when testing application
+    /// </summary>
+    /// <param name="configuration"></param>
+    /// <param name="testEnvironmentName">The test environment name</param>
+    /// <returns></returns>
+    public static ILogger CreateLogger(this LoggerConfiguration configuration, string testEnvironmentName = "test")
+    {
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        
+        if (testEnvironmentName.Equals(env, StringComparison.InvariantCultureIgnoreCase))
+            return configuration.CreateLogger();
+
+        return configuration.CreateBootstrapLogger();
+    }
+}
